28ee98a74011ed1f359721f1ebcb8e1b3e4324d0
Sara Davis <50930477+SaraDavis1991@users.noreply.github.com>
2022-06-15 17:52:33 -0700

    Update driver.cpp

--- driver.cpp
+++ driver.cpp
@@ -6,18 +6,7 @@ int main(){
 
     Meat doublePatty("Double Patty", "Beef", 2);
     Hamburger doubleBurger("Double Beef Burger" , doublePatty, false, true, true);
-    doubleBurger.printSandwich();
-    doubleBurger.printHamburger();
-    doublePatty.printMeat();
 
-    cout << endl << endl;
-
-    doublePatty.setType("Chicken");
-    doubleBurger.setMeatType(doublePatty);
-    
-    doubleBurger.printSandwich();
-    doubleBurger.printHamburger();
-    doublePatty.printMeat();
 
 
     return 0;
